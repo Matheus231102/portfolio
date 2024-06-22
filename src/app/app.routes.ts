877d7fb59e101fd649515ff6b5703d10bb1c5f06
Matheus de Souza Badia <matheusbadia23@gmail.com>
2024-06-21 23:26:52 -0300

    first page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {InitialPageComponent} from "./components/pageComponents/initial-page/initial-page.component";
+import {ContactPageComponent} from "./components/pageComponents/contact-page/contact-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: InitialPageComponent },
+  { path: "contact", component: ContactPageComponent },
+];
